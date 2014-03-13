5b24b233f57f5936b320c406b5369321f1984de6
Evgueni Ratchev <eratchev@yahoo.com>
2014-03-13 13:19:48 -0700

    adding scalatra rev 2 - json APIs

--- scalatra/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ scalatra/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -2,6 +2,11 @@ package com.example.app
 
 import org.scalatra._
 import scalate.ScalateSupport
+import scala.util.parsing.json
+import main.scala.com.example.app.JsonUtil
+
+case class Payload(str: String)
+case class Payload2(string: String)
 
 class MyScalatraServlet extends ScalatraStack {
 
@@ -19,6 +24,17 @@ class MyScalatraServlet extends ScalatraStack {
     "hello world"
   }
 
+  post("/v1/json") {
+    val in = JsonUtil.fromJson[Payload](request.body)
+    val out = Payload(in.str.toUpperCase)
+    JsonUtil.toJson(out)
+  }
 
+  post("/v2/json") {
+    val in = JsonUtil.fromJson[Payload2](request.body)
+    val out = Payload2(in.string.toUpperCase)
+    Thread.sleep(50)
+    JsonUtil.toJson(out)
+  }
 
 }
