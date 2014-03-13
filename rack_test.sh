0fa86ff09cd59bf41334e9fdf989ec7c22598b7c
Tejas Mandke <tejas.mandke@workday.com>
2014-03-13 10:52:13 -0700

    change scripts to be genral

--- rack_test.sh
+++ rack_test.sh
@@ -4,7 +4,7 @@ rbenv shell 2.1.1
 cd $1
 bundle
 echo Running C Ruby 2.1.1
-bundle exec puma -p 8080 & pid=$!
+../bin/ruby-puma & pid=$!
 sleep 10
 cd -
 ./test.sh $1-$2-cruby-puma
@@ -12,7 +12,7 @@ cd -
 kill $pid
 
 cd $1
-bundle exec unicorn -p 8080 -E production & pid=$!
+../bin/ruby-unicorn & pid=$!
 cd -
 sleep 10
 ./test.sh $1-$2-cruby-unicorn
@@ -23,8 +23,8 @@ rbenv shell jruby-1.7.11
 echo Running Jruby 1.7.11
 cd $1
 bundle
-bundle exec puma -p 8080 & pid=$!
-sleep 10
+../bin/jruby-puma & pid=$!
+sleep 20
 cd -
 ./test.sh $1-$2-jruby-puma
 kill $pid
