673b02004b4d640c1cec4ff4101c3087be342490
Tejas Mandke <tejas.mandke@workday.com>
2014-03-12 17:50:15 -0700

    fix script

--- rack_test.sh
+++ rack_test.sh
@@ -1,7 +1,32 @@
 #!/bin/bash -x -e
-read -p "Start C-ruby puma Server"
-./test.sh $1 $1-cruby-puma
-read -p "Start C-ruby unicorn Server"
-./test.sh $1 $1-cruby-unicorn
-read -p "Start Jruby puma Server"
-./test.sh $1 $1-jruby-puma
+eval "$(rbenv init -)"
+rbenv shell 2.1.1
+cd $1
+bundle
+echo Running C Ruby 2.1.1
+bundle exec puma -p 8080 & pid=$!
+sleep 10
+cd -
+./test.sh $1-$2-cruby-puma
+
+kill $pid
+
+cd $1
+bundle exec unicorn -p 8080 -E production & pid=$!
+cd -
+sleep 10
+./test.sh $1-$2-cruby-unicorn
+kill $pid
+
+rbenv shell jruby-1.7.11  
+
+echo Running Jruby 1.7.11
+cd $1
+bundle
+bundle exec puma -p 8080 & pid=$!
+sleep 10
+cd -
+./test.sh $1-$2-jruby-puma
+kill $pid
+
+tail -n 20 results/$1-$2*
