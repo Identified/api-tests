ec3f2923d28ef328f81489bcbb8f6e117de322a8
Andrew Bullen <andrew.bullen@workday.com>
2014-03-13 14:47:52 -0700

    make spray json apis work

--- spray/src/main/scala/com/example/JsonService.scala
+++ spray/src/main/scala/com/example/JsonService.scala
@@ -3,15 +3,34 @@ package com.example
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import spray.json._
+import spray.httpx.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
 
 // this trait defines our service behavior independently from the service actor
 trait JsonService extends HttpService {
 
+  case class V1Word(str: String)
+  case class V2Word(string: String)
+
   val jsonRoute =
-    path(Segment / "json") { pathVersion: String =>
+    path(Segment / "json") { (version: String) =>
       post {
-        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
-          complete {"something"}
+        entity(as[Map[String, String]]) { input =>
+
+          respondWithMediaType(`application/json`) {
+            var key = version match {
+              case "v1" => "str"
+              case "v2" =>"string"
+            }
+
+            val response = input(key).toUpperCase
+
+            complete{
+              JsObject(key -> JsString(response))
+            }
+          }
         }
       }
     }
--- spray/src/test/scala/com/example/JsonServiceSpec.scala
+++ spray/src/test/scala/com/example/JsonServiceSpec.scala
@@ -4,6 +4,7 @@ import org.specs2.mutable.Specification
 import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
+import spray.httpx._
 
 class JsonServiceSpec extends Specification with Specs2RouteTest with JsonService {
   def actorRefFactory = system
@@ -11,14 +12,18 @@ class JsonServiceSpec extends Specification with Specs2RouteTest with JsonServic
   "JsonService" should {
 
     "return something for POST requests to the v1/json path" in {
-      Post("/v1/json") ~> jsonRoute ~> check {
-        responseAs[String] must contain("something")
+      Post("/v1/json",
+        HttpEntity(MediaTypes.`application/json`, """{"str":"something"}""")
+      ) ~> jsonRoute ~> check {
+        responseAs[String] must contain("SOMETHING")
       }
     }
 
     "return something for POST requests to the v2/json path" in {
-      Post("/v2/json") ~> jsonRoute ~> check {
-        responseAs[String] must contain("something")
+      Post("/v2/json",
+        HttpEntity(MediaTypes.`application/json`, """{"string":"something"}""")
+      ) ~> jsonRoute ~> check {
+        responseAs[String] must contain("SOMETHING")
       }
     }
   }
